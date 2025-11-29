fb1e4dc1727f81e276d65a84dff988b676713090
DoggoGood <kammat006@wp.pl>
2025-11-29 18:28:51 +0100

    fixed graph generation

--- main.c
+++ main.c
@@ -10,10 +10,14 @@ int main() {
     srand(time(NULL));
     int n, m;
     char *method;
+    printf("Podaj liczbę wierzchołków, liczbę krawędzi i metodę reprezentacji "
+           "grafu (ms - macierz sąsiedztwa, mi - macierz incydencji, ls - "
+           "lista sąsiedztwa), np. 4 3 ms:\n");
     scanf("%d %d %ms", &n, &m, &method);
     // Sprawdzanie czy graf jest spójny na wejściu
     if (m < n - 1) {
         fprintf(stderr, "Za mało krawędzi, aby graf był spójny\n");
+        free(method);
         return 1;
     }
     if (strcmp(method, "ms") == 0)
